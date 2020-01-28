b85a7b44f60977d99b3a949174d21f81ae780421
avinashdevicode <avinashdevicode@gmail.com>
2020-01-28 23:30:11 +0000

    Dropdowns

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var app = angular.module('plunker', ['ngMaterial', 'ngAria', 'ngRoute', 'ngAnimate', 'ngSanitize']);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.name = 'World';
+  $scope.categories = ["Useful for process","Non Sensical","Adultry Content",
+'Not Relevent for Analysis', 'Other'];
+$scope.intents = ["Location",
+"Business", 'Food Recipes','Movies', 'Product', 'Videos', 'imagees', "Answers", 'Other'];
+$scope.richRatings = ["Good Abandonment",
+"Attractive card", 'Unattractive','Rich card is not relevant'];
+$scope.allRatings = ["0", "1","2", "3","4", "5"];
+$scope.deviationCategories = ['Data Present', 'Data not present', 'Spelling Difference',
+ 'Address missmatch','Sentence Structure in wrong']
+});
