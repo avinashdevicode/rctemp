b7cb46d874364c5adc3e9fc566f7d35601300a39
avinashdevicode <avinashdevicode@gmail.com>
2020-02-11 15:08:28 +0000

    adding submit functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
 var app = angular.module('plunker', ['ngMaterial', 'ngAria', 'ngRoute', 'ngAnimate', 'ngSanitize']);
 
-app.controller('MainCtrl', function($scope) {
+app.controller('MainCtrl', function ($scope) {
   $scope.name = 'World';
-  $scope.categories = ["Useful for process","Non Sensical","Adultry Content",
-'Not Relevent for Analysis', 'Other'];
-$scope.intents = ["Location",
-"Business", 'Food Recipes','Movies', 'Product', 'Videos', 'imagees', "Answers", 'Other'];
-$scope.richRatings = ["Good Abandonment",
-"Attractive card", 'Unattractive','Rich card is not relevant'];
-$scope.richcardCategories = ['Article','Book','Course','Review','Event','FAQ','Local Business','organization','Movie','Product','Recipe','Site Search','Image details','Video','Question & Answer'];
+  $scope.categories = ["Useful for process", "Non Sensical", "Adultry Content",
+    'Not Relevent for Analysis', 'Other'
+  ];
+  $scope.intents = ["Location",
+    "Business", 'Food Recipes', 'Movies', 'Product', 'Videos', 'imagees', "Answers", 'Other'
+  ];
+  $scope.richRatings = ["Good Abandonment",
+    "Attractive card", 'Unattractive', 'Rich card is not relevant'
+  ];
+  $scope.richcardCategories = ['Article', 'Book', 'Course', 'Review', 'Event', 'FAQ', 'Local Business', 'organization', 'Movie', 'Product', 'Recipe', 'Site Search', 'Image details', 'Video', 'Question & Answer'];
 
-$scope.allRatings = ["0", "1","2", "3","4", "5"];
-$scope.deviationCategories = ['Data Present', 'Data not present', 'Spelling Difference',
- 'Address missmatch','Sentence Structure in wrong']
+  $scope.allRatings = ["0", "1", "2", "3", "4", "5"];
+  $scope.deviationCategories = ['Data Present', 'Data not present', 'Spelling Difference',
+    'Address missmatch', 'Sentence Structure in wrong'
+  ]
 });
+
+$("#menu-toggle").click(function (e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
+
+function returnTo(page) {
+  window.location.href = '/' + page + '.html', true;
+};
