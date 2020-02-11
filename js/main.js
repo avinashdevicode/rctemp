e9e55af47fb08798945e10f02ee5fc1a765b5479
avinashdevicode <avinashdevicode@gmail.com>
2020-02-11 14:08:45 +0000

    adding Card  Category

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $scope.intents = ["Location",
 "Business", 'Food Recipes','Movies', 'Product', 'Videos', 'imagees', "Answers", 'Other'];
 $scope.richRatings = ["Good Abandonment",
 "Attractive card", 'Unattractive','Rich card is not relevant'];
+$scope.richcardCategories = ['Article','Book','Course','Review','Event','FAQ','Local Business','organization','Movie','Product','Recipe','Site Search','Image details','Video','Question & Answer'];
+
 $scope.allRatings = ["0", "1","2", "3","4", "5"];
 $scope.deviationCategories = ['Data Present', 'Data not present', 'Spelling Difference',
  'Address missmatch','Sentence Structure in wrong']
